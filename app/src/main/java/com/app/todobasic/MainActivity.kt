8abfe239ca5be96856a8b9607651712e8606abfd
blackinitial <alhanif.blackinitial@gmail.com>
2022-04-17 21:47:09 +0700

    add logic function

--- app/src/main/java/com/app/todobasic/MainActivity.kt
+++ app/src/main/java/com/app/todobasic/MainActivity.kt
@@ -2,10 +2,33 @@ package com.app.todobasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_todo.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        todoItems.adapter = todoAdapter
+        todoItems.layoutManager = LinearLayoutManager(this)
+
+        todoBtnAdd.setOnClickListener {
+            val todoValInput = todoInput.text.toString()
+            if(todoValInput.isNotEmpty()) {
+                val todo = Todo(todoValInput)
+                todoAdapter.addTodo(todo)
+                todoInput.text.clear()
+            }
+        }
+        todoBtnDelete.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
+
     }
 }
---
+++ app/src/main/java/com/app/todobasic/Todo.kt
@@ -0,0 +1,6 @@
+package com.app.todobasic
+
+class Todo (
+    val title: String,
+    var isChecked: Boolean = false
+)
